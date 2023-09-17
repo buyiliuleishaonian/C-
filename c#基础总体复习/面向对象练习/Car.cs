fc139eefd71ce61aa3a448311f432a237d3cc865
student <2424215016@qq.com>
2023-09-17 20:52:26 +0800

    frist

---
+++ c#基础总体复习/面向对象练习/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象练习
+{
+    public  class Car:Vehicle
+    {
+        public int Person
+        {
+            get;
+            set;
+        }
+        public Car(string color ,string bland,int person):base(color,bland)
+        {
+            this.Person = person;
+        }
+        public void passenger()
+        {
+            Console.WriteLine("可以最多载{1}人",this.Person);
+        }
+    }
+}
